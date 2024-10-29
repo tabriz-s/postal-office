49c236e47e86ab14db3d3e96cf8d84eb685549b4
Tabriz Sadredinov <111303283+tabriz-s@users.noreply.github.com>
2024-10-29 01:45:28 -0500

    update-refunds

---
+++ COSCPFWA/COSCPFWA/Refunds.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Title="Refunds" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Refunds.aspx.cs" Inherits="COSCPFWA.Refunds" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <main aria-labelledby="title">
+        <h1>Refunds</h1>
+
+        <!-- Filter by Refund Status -->
+        <div class="mt-3">
+            <label for="statusFilter">Filter by Status:</label>
+            <asp:DropDownList ID="ddlStatusFilter" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlStatusFilter_SelectedIndexChanged">
+                <asp:ListItem Text="All" Value="All" />
+                <asp:ListItem Text="Pending" Value="Pending" />
+                <asp:ListItem Text="Approved" Value="Approved" />
+            </asp:DropDownList>
+        </div>
+
+        <!-- Display Refunds in a Table -->
+        <asp:Repeater ID="refundRepeater" runat="server">
+            <HeaderTemplate>
+                <table class="table table-striped mt-4">
+                    <thead>
+                        <tr>
+                            <th>Refund ID</th>
+                            <th>Customer Name</th>
+                            <th>Package ID</th>
+                            <th>Refund Amount</th>
+                            <th>Refund Reason</th>
+                            <th>Refund Date</th>
+                            <th>Refund Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <tr>
+                    <td><%# Eval("RefundID") %></td>
+                    <td><%# Eval("CustomerName") %></td>
+                    <td><%# Eval("PackageID") %></td>
+                    <td><%# Eval("RefundAmount", "{0:C}") %></td>
+                    <td><%# Eval("RefundReason") %></td>
+                    <td><%# Eval("RefundDate", "{0:yyyy-MM-dd}") %></td>
+                    <td><%# Eval("RefundStatus") %></td>
+                    <td>
+                        <asp:Button ID="btnApprove" runat="server" Text="Approve" CssClass="btn btn-sm btn-success" CommandArgument='<%# Eval("RefundID") %>' OnClick="btnApprove_Click" />
+                    </td>
+                </tr>
+            </ItemTemplate>
+            <FooterTemplate>
+                </tbody>
+        </table>
+   
+            </FooterTemplate>
+        </asp:Repeater>
+
+        <!-- Total Refunds Section -->
+        <div class="mt-4">
+            <asp:Label ID="lblTotalRefund" runat="server" Text="Total Refund Amount: " CssClass="font-weight-bold"></asp:Label>
+            <asp:Label ID="lblRefundSum" runat="server" CssClass="font-weight-bold"></asp:Label>
+        </div>
+    </main>
+</asp:Content>
---
+++ COSCPFWA/COSCPFWA/Refunds.aspx.cs
@@ -0,0 +1,119 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace COSCPFWA
+{
+    public partial class Refunds : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRefunds("All");  // Load all refunds by default
+                CalculateTotalRefund();
+            }
+        }
+
+        protected void LoadRefunds(string statusFilter)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                string query = "SELECT r.RefundID, CONCAT(c.FirstName, ' ', c.LastName) AS CustomerName, " +
+                               "r.PackageID, r.RefundAmount, r.RefundReason, r.RefundDate, r.RefundStatus " +
+                               "FROM refunds r JOIN customer c ON r.CustomerID = c.CustomerID";
+
+                if (statusFilter != "All")
+                {
+                    query += " WHERE r.RefundStatus = @StatusFilter";
+                }
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    if (statusFilter != "All")
+                    {
+                        cmd.Parameters.AddWithValue("@StatusFilter", statusFilter);
+                    }
+
+                    using (MySqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        refundRepeater.DataSource = dt;
+                        refundRepeater.DataBind();
+                    }
+                }
+            }
+        }
+
+        protected void ddlStatusFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedStatus = ddlStatusFilter.SelectedValue;
+            LoadRefunds(selectedStatus);
+            CalculateTotalRefund();
+        }
+
+        protected void btnApprove_Click(object sender, EventArgs e)
+        {
+            Button btnApprove = (Button)sender;
+            string refundID = btnApprove.CommandArgument;
+
+            string connString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "UPDATE refunds SET RefundStatus = 'Approved' WHERE RefundID = @RefundID";
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@RefundID", refundID);
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    LoadRefunds(ddlStatusFilter.SelectedValue); // Reload refunds to reflect changes
+                    CalculateTotalRefund(); // Recalculate total refund amount
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<script>alert('Error approving refund: " + ex.Message + "');</script>");
+                }
+            }
+        }
+
+        protected void CalculateTotalRefund()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                conn.Open();
+                string query = "SELECT SUM(RefundAmount) FROM refunds";
+
+                if (ddlStatusFilter.SelectedValue != "All")
+                {
+                    query += " WHERE RefundStatus = @StatusFilter";
+                }
+
+                using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                {
+                    if (ddlStatusFilter.SelectedValue != "All")
+                    {
+                        cmd.Parameters.AddWithValue("@StatusFilter", ddlStatusFilter.SelectedValue);
+                    }
+
+                    object result = cmd.ExecuteScalar();
+                    decimal totalRefund = result != DBNull.Value ? Convert.ToDecimal(result) : 0;
+                    lblRefundSum.Text = $"{totalRefund:C}";
+                }
+            }
+        }
+    }
+}
